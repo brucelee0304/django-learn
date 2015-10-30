c992e60d37ff688ae048e82945397bc6aef9a0b8
zipu888 <285764190@qq.com>
2015-10-30 11:16:21 +0800

    add _unicode_ func,this func is just like toString func in java

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,10 +6,16 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    #just like toString in java
+    def __unicode__(self):
+        print self.question
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        print self.choice_text
 
